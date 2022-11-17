5a74340502aacaa0a27e106e558115db3a03d5e5
User <User@DESKTOP-OFNSVN2>
2022-11-17 18:25:43 -0500

    Add project files.

---
+++ BlazorApp/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorApp
+{
+    public class TodoItem
+    {
+        public string? Title { get; set; }
+        public bool isDone { get; set; } = false;
+    }
+}
+
+
+// https://learn.microsoft.com/en-us/training/modules/build-blazor-webassembly-visual-studio-code/7-exercise-razor-binding?pivots=vstudio 
