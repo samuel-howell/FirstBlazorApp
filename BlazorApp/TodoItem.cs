dec2bc30be3700c9f899038f251bc75106c4eafc
User <User@DESKTOP-OFNSVN2>
2022-11-21 20:16:50 -0500

    added bind controls

--- BlazorApp/TodoItem.cs
+++ BlazorApp/TodoItem.cs
@@ -4,8 +4,9 @@
     {
         public string? Title { get; set; }
         public bool isDone { get; set; } = false;
+
     }
 }
 
 
-// https://learn.microsoft.com/en-us/training/modules/build-blazor-webassembly-visual-studio-code/7-exercise-razor-binding?pivots=vstudio 
+// https://learn.microsoft.com/en-us/training/modules/interact-with-data-blazor-web-apps/  
